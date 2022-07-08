dec53948374304a4907084715ffe6eeead7a07c6
P. S. Clarke <downpage@pscl4rke.net>
2022-07-08 12:35:27 +0100

    Improve test coverage

--- downpage/generating.py
+++ downpage/generating.py
@@ -11,10 +11,9 @@ from django.template.loader import render_to_string
 
 
 def generate():
-    if not hasattr(settings, "DOWNPAGE_PAGES"):
-        raise ImproperlyConfigured("Missing DOWNPAGE_PAGES")
     LOG.debug("Started")
-    for filename, templatename in settings.DOWNPAGE_PAGES:
+    pages_to_generate = get_pages_to_generate(settings)
+    for filename, templatename in pages_to_generate:
         filepath = os.path.join(settings.STATIC_ROOT, filename)
         LOG.debug("Building %r..." % filepath)
         context = {"request": None}
@@ -22,3 +21,9 @@ def generate():
         with open(filepath, "w") as destfile:
             destfile.write(content)
     LOG.debug("Finished")
+
+
+def get_pages_to_generate(settings):
+    if not hasattr(settings, "DOWNPAGE_PAGES"):
+        raise ImproperlyConfigured("Missing DOWNPAGE_PAGES")
+    return settings.DOWNPAGE_PAGES
--- tests/test_generating.py
+++ tests/test_generating.py
@@ -5,7 +5,18 @@ import unittest
 from downpage import generating
 
 
-class TestImporting(unittest.TestCase):
+class TestListOfPages(unittest.TestCase):
 
-    def test_nothing(self):
-        pass
+    def test_when_misconfigured(self):
+        class Settings:
+            pass  # not set
+        with self.assertRaises(generating.ImproperlyConfigured):
+            generating.get_pages_to_generate(Settings())
+
+    def test_getting_pages(self):
+        class Settings:
+            DOWNPAGE_PAGES = [("foo", "bar")]
+        pages = generating.get_pages_to_generate(Settings())
+        self.assertEqual(len(pages), 1)
+        self.assertEqual(pages[0][0], "foo")
+        self.assertEqual(pages[0][1], "bar")
